062fc80738636f48bfcdc43ef4611957c73f171f
Mattias Djärv <djarvm@chalmers.se>
2024-05-23 17:02:45 +0200

    started with solver

--- solver.cpp
+++ solver.cpp
@@ -4,16 +4,31 @@
 #include <fstream>
 #include <unordered_map>
 #include <list>
+#include <bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
 
 void readfile(string filename);
+void evaluate_words();
+void sort(map<string, double> &M);
+bool cmp(pair<string, double> &a,
+         pair<string, double> &b);
 
-unordered_map<char, int[5]> freq;
-unordered_map<char, unordered_map<char, int[5]>> preceeding_letter;
-unordered_map<char, unordered_map<char, int[5]>> succeeding_letter;
-list<string> guesses;
-list<char> alphabet { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' }
+unordered_map<char, double[5]> freq;
+unordered_map<char, unordered_map<char, double[5]>> preceeding_letter;
+unordered_map<char, unordered_map<char, double[5]>> succeeding_letter;
+string guesses[7];
+string results[7][5] = {
+    {"Nope", "Nope", "Nope", "Nope", "Nope"},
+    {"Nope", "Nope", "Nope", "Nope", "Nope"},
+    {"Nope", "Nope", "Nope", "Nope", "Nope"},
+    {"Nope", "Nope", "Nope", "Nope", "Nope"},
+    {"Nope", "Nope", "Nope", "Nope", "Nope"},
+    {"Nope", "Nope", "Nope", "Nope", "Nope"},
+    {"Nope", "Nope", "Nope", "Nope", "Nope"}};
+list<char> alphabet{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+map<string, double> scores;
 int main()
 {
 
@@ -24,6 +39,8 @@ int main()
 
     while (true)
     {
+        evaluate_words();
+        sort(scores);
         guesscount++;
         cout << freq['Q'][2];
         cout << "hello worldle! Type guess number " << guesscount << '\n';
@@ -38,33 +55,47 @@ int main()
         for (auto &c : guess)
             c = toupper(c);
         cout << "You guessed: " << guess << '\n';
-        guesses.push_back(guess);
+        guesses[guesscount - 1] = guess;
 
-        int wordindex = 0;
-
-        string result[5] = {"Nope", "Nope", "Nope", "Nope", "Nope"};
-        for (char i : secret)
+        for (int i = 0; i < 2; i++)
         {
             int guessindex = 0;
-            for (char j : guess)
+            for (char guess_c : guess)
             {
-                if (i == j)
+                int secretindex = 0;
+                bool correct_letter = false;
+                for (char secret_c : secret)
                 {
-                    if (wordindex == guessindex)
+                    if (secret_c == guess_c)
                     {
-                        result[wordindex] = "green";
+                        correct_letter = true;
+                        if (secretindex == guessindex)
+                        {
+                            results[guesscount][secretindex] = "green";
+                        }
+                        else if (i == 1)
+                        {
+                            results[guesscount][guessindex] = "yellow";
+                        }
                     }
-                    else
-                    {
-                        result[guessindex] = "yellow";
-                    }
+                    secretindex++;
+                }
+                if (!correct_letter)
+                {
+
+                    alphabet.remove(guess_c);
                 }
                 guessindex++;
             }
-            wordindex++;
         }
+
         cout << "Result of: " << guess << '\n';
-        for (string i : result)
+
+        for (string i : results[guesscount])
+        {
+            cout << i << ", ";
+        }
+        for (char i : alphabet)
         {
             cout << i << ", ";
         }
@@ -84,7 +115,7 @@ void readfile(string filename)
         { // read data from file object and put it into string.
             int i = 0;
             char prec = ' ';
-            char suc = ' ';
+
             for (char c : tp)
             {
                 freq[toupper(c)][i]++;
@@ -94,11 +125,89 @@ void readfile(string filename)
                     preceeding_letter[toupper(c)][toupper(prec)][i]++;
                     succeeding_letter[toupper(prec)][toupper(c)][i - 1]++;
                 }
+                scores[tp] = 0;
 
                 prec = c;
                 i++;
             }
         }
         newfile.close(); // close the file object.
+    }
+}
+
+void evaluate_words()
+{
+    list<string> impossiblewords;
+    for (auto const &[word, _] : scores)
+    {
+        int i = 0;
+        double score = 0;
+        char prec = ' ';
+        bool found;
+        for (char c : word)
+        {
+            c = toupper(c);
+            found = (find(alphabet.begin(), alphabet.end(), c) != alphabet.end());
+            if (!found)
+            {
+                score -= 500000;
+            }
+
+            if (i > 0)
+            {
+                score += preceeding_letter[c][prec][i];
+                score += succeeding_letter[prec][c][i - 1];
+            }
+
+            score += freq.at(c)[i] / 2;
+            i++;
+            prec = c;
+            scores[word] = score;
+        }
+        if (score < 0)
+        {
+            impossiblewords.push_back(word);
+        }
+    }
+    for (string delword : impossiblewords)
+    {
+        scores.erase(delword);
+    }
+}
+
+bool cmp(pair<string, double> &a,
+         pair<string, double> &b)
+{
+    return a.second > b.second;
+}
+
+// Function to sort the map according
+// to value in a (key-value) pairs
+void sort(map<string, double> &M)
+{
+
+    // Declare vector of pairs
+    vector<pair<string, double>> A;
+
+    // Copy key-value pair from Map
+    // to vector of pairs
+    for (auto &it : M)
+    {
+        A.push_back(it);
+    }
+
+    // Sort using comparator function
+    sort(A.begin(), A.end(), cmp);
+
+    int printnum = 0;
+    // Print the sorted value
+    for (auto &it : A)
+    {
+
+        cout << it.first << ' '
+             << it.second << endl;
+        printnum++;
+        if (printnum >= 5)
+            break;
     }
 }
